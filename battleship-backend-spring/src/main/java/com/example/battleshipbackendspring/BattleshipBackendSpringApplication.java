2ad1e6ab0bf116b300d2d8a7f24d7fb1c0ac730d
Djolee00 <ivanovic.djordje2019@gmail.com>
2022-08-07 19:07:11 +0200

    First commit

---
+++ battleship-backend-spring/src/main/java/com/example/battleshipbackendspring/BattleshipBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.battleshipbackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipBackendSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BattleshipBackendSpringApplication.class, args);
+    }
+
+}
---
+++ battleship-backend-spring/src/test/java/com/example/battleshipbackendspring/BattleshipBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.battleshipbackendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipBackendSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

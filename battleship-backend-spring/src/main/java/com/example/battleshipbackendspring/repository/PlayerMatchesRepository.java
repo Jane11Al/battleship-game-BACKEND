e2ffd17bf847e2c5d6a0013af60964f85f0e9130
Djolee00 <ivanovic.djordje2019@gmail.com>
2022-08-08 17:38:05 +0200

    Added PlayerMatches repository

---
+++ battleship-backend-spring/src/main/java/com/example/battleshipbackendspring/repository/PlayerMatchesRepository.java
@@ -0,0 +1,11 @@
+package com.example.battleshipbackendspring.repository;
+
+import com.example.battleshipbackendspring.model.PlayerMatches;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PlayerMatchesRepository extends MongoRepository<PlayerMatches,String> {
+
+    PlayerMatches findByPlayer1(String player1);
+
+    PlayerMatches findByWebSocketAddress(String webSocketAddress);
+}
